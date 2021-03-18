7924ac059d0e447542bffaadb54a6fa58856b2e9
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-18 18:31:18 -0500

    Create descendingOrder.js
    Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.

---
+++ descendingOrder.js
@@ -0,0 +1,8 @@
+function descendingOrder(n){
+  //...
+  let numbers = (n + "").split("").sort(function(a, b) {
+    return b - a
+  });
+  numbers = numbers.join("");
+    return Number(numbers);
+}

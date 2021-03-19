023f7a7aa7f4d86e286dda1260b421cee8620ad8
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-18 22:12:39 -0500

    Update findTheParityOutlier.js
    This other solution (below) looks a lot cleaner than mines

--- findTheParityOutlier.js
+++ findTheParityOutlier.js
@@ -15,3 +15,14 @@ function findOutlier(integers){
   
   return oddArr.length === 1 ? oddArr[0] : evenArr[0]
 };
+
+
+/*
+This other solution (below) looks a lot cleaner than mines :(
+*/
+
+function findOutlier(integers){
+  const even = integers.filter(int => int % 2 === 0);
+  const odd  = integers.filter(int => int % 2 !== 0);
+  return even.length === 1 ? even[0] : odd[0];
+}

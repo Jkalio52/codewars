2d2188aef1ed68a8d553abfc50fb14298f4ca57f
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-21 22:00:54 -0500

    Create sort_a_given_string.js
    Your order, please!
    
    Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
    
    Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
    
    If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
    
    Examples:
    "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
    "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
    ""  -->  ""

---
+++ sort_a_given_string.js
@@ -0,0 +1,15 @@
+function order(words){
+  // ...
+  let wordsArray = words.split(' ');
+  let numArray = words.match(/[1-9]/g);
+  let newArray = [];
+  if (words.length == 0) {
+    return words
+  }
+  
+  for (let i = 1; i <= numArray.length; i++) {
+    let num = numArray.indexOf(i.toString())
+    newArray.push(wordsArray[num])
+  }
+  return newArray.join(' ')
+}

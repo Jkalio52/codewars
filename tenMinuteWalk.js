22c7ba567b6b633e8673e6d586c2685a8671620f
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-23 22:43:43 -0500

    Update tenMinuteWalk.js

--- tenMinuteWalk.js
+++ tenMinuteWalk.js
@@ -20,3 +20,16 @@ function isValidWalk(walk) {
     return true;
   }
 }
+
+
+
+
+//...
+function isValidWalk(walk) {
+  const north = walk.filter(item => { return item === "n" }).length;
+  const south = walk.filter(item => { return item === "s" }).length;
+  const east = walk.filter(item => { return item === "e" }).length;
+  const west = walk.filter(item => { return item === "w" }).length;
+  
+  return walk.length === 10 && north === south && east === west;
+}

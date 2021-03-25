944217393476fa445eacc7dff1e49b7347ca3fa7
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-24 20:35:46 -0500

    Create Array_diff.js
    Array.diff
    
    Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
    
    It should remove all values from list a, which are present in list b.
    
    arrayDiff([1,2],[1]) == [2]
    If a value is present in b, all of its occurrences must be removed from the other:
    
    arrayDiff([1,2,2,2,3],[2]) == [1,3]
    
    
    Tags: FUNDAMENTALS ARRAYS

---
+++ Array_diff.js
@@ -0,0 +1,40 @@
+function arrayDiff(a, b) {
+  let diff = a.filter(x => b.indexOf(x) === -1)
+    return diff;
+}
+
+
+
+//...
+function arrayDiff(a, b) {
+  let diff = [];
+  a = a.toString().split(',').map(Number);
+  b = b.toString().split(',').map(Number);
+
+  for (let i in a) {
+    if (b.indexOf(a[i]) === 1) diff.push(a[i]);
+  }
+  for (i in b) {
+    if (a.indexOf(b[i]) === 1) diff.push(b[i]);
+  }
+  return diff.sort((a, b) => a - b);
+}
+
+
+
+//....
+function arrayDiff(a, b) {
+  let diff = [];
+  for (let i = 0; i < a.length; i++) {
+    if (b.indexOf(a[i]) === -1) {
+      diff.push(a[i]);
+    }
+  }
+  for (i = 0; i < b.length; i++) {
+    if (a.indexOf(b[i]) === 1) {
+      diff.push(b[i]);
+    }
+  }
+  return diff;
+}
+

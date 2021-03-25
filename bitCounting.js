b12924145e0dc3fc97669740299413a34236162f
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-25 02:07:02 -0500

    Create bitCounting.js
    Bit Counting!!!
    
    Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
    
    Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
    
    Tags: ALGORITHMSBITSBINARY

---
+++ bitCounting.js
@@ -0,0 +1,10 @@
+var countBits = function(n) {
+  // Program Me
+  // Create an array with bit output
+   let binary = (n).toString(2).split('');
+   
+   // Then create a sum with the array and make the index a Number
+   let output = binary.reduce((sum, num) => sum + Number(num), 0);
+   
+   return output;
+};

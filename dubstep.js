4737d0dba760eb39f24ac73c55c9d56ce55299be
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-26 01:26:01 -0500

    Update dubstep.js
    Edit code...

--- dubstep.js
+++ dubstep.js
@@ -1,12 +1,12 @@
 function songDecoder(song){
   // At its simplest form...
   return (
-		song
-			// First, split strings into array
-			.split('WUB')
-			// Second, remove empty strings, with (' ')
-			.filter(Boolean)
-			// And finally, join the words from the array into a string
-			.join(' ')
-	  );
+	song
+	  // First, split strings into array
+	  .split('WUB')
+      	  // Second, remove empty strings, with (' ')
+	  .filter(Boolean)
+	  // And finally, join the words from the array into a string
+	  .join(' ')
+  );
 }

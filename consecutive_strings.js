275031c072fe4d941a7e0156ec449b6cb4168e26
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-27 01:07:50 -0500

    Create consecutive_strings.js
    Consecutive Strings!
    
    You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
    
    Examples:
    strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
    
    Concatenate the consecutive strings of strarr by 2, we get:
    
    treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
    folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
    trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
    blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
    abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
    
    Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
    The first that came is "folingtrashy" so 
    longest_consec(strarr, 2) should return "folingtrashy".
    
    In the same way:
    longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
    n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
    
    Note:
    consecutive strings: follow one after another without any interruption
    
    Tags: FUNDAMENTALS

---
+++ consecutive_strings.js
@@ -0,0 +1,17 @@
+function longestConsec(strarr, k) {
+    // your code
+  if (strarr.length == 0 || k > strarr.length || k <= 0) return '';
+
+  let longestString = '';
+
+  let newString = '';
+
+  for (let i = 0; i < strarr.length; i++) {
+    newString = strarr.slice(i, i + k);
+    if (newString.join('').length > longestString.length ) {
+      longestString = newString.join('');
+    }
+  }
+
+  return longestString;
+}

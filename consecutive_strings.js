5949395f06780f5708a38254771ced1f801fa363
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-27 01:10:36 -0500

    Update consecutive_strings.js
    Edit/correct errors on line 9

--- consecutive_strings.js
+++ consecutive_strings.js
@@ -1,17 +1,16 @@
 function longestConsec(strarr, k) {
     // your code
-  if (strarr.length == 0 || k > strarr.length || k <= 0) return '';
+    if (strarr.length == 0 || k > strarr.length || k <= 0) return '';
 
-  let longestString = '';
+    let longestString = '';
+    let newString = '';
 
-  let newString = '';
-
-  for (let i = 0; i < strarr.length; i++) {
-    newString = strarr.slice(i, i + k);
-    if (newString.join('').length > longestString.length ) {
-      longestString = newString.join('');
+    for (let i = 0; i < strarr.length; i++) {
+        newString = strarr.slice(i, i + k);
+        if (newString.join('').length > longestString.length ) {
+        longestString = newString.join('');
+        }
     }
-  }
 
-  return longestString;
+    return longestString;
 }

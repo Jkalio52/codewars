a386c975447b29d42c5d1bcf5a1dd56a8de84d61
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-27 01:15:42 -0500

    Update consecutive_strings.js
    Add alternative solution, clever!

--- consecutive_strings.js
+++ consecutive_strings.js
@@ -14,3 +14,15 @@ function longestConsec(strarr, k) {
 
     return longestString;
 }
+
+
+
+
+// A clever solution ...I like this :)
+// I got lost at line #25, but I got it now lol
+function longestConsec(strarr, k) {
+  if( strarr.length==0 || k> strarr.length || k <1 ) return "";
+  let lens = strarr.map( (_,i,arr) => arr.slice(i,i+k).join('').length ),
+      i = lens.indexOf( Math.max(...lens) );
+  return strarr.slice(i,i+k).join('')
+}

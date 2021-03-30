3d53119ca313eb5a30854aee86504b03d392d261
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-29 21:37:34 -0500

    Update twice_linear.js
    Add a second solution...

--- twice_linear.js
+++ twice_linear.js
@@ -1,5 +1,6 @@
 function dblLinear(n) {
     // your code
+    // 
   let arr = [1], x = 0, y = 0
     for (let i = 0; i < n; i++) {
         let arrStart = 2 * arr[x] + 1, arrEnd = 3 * arr[y] + 1
@@ -15,4 +16,22 @@ function dblLinear(n) {
     }
   
     return arr[n]
+}
+
+
+
+
+// Solution #2
+function dblLinear(n) {
+    //
+    let arr = [1], x = 0, y = 0;
+    
+    for (let i = 1; i <= n; i++) {
+      arr[i] = Math.min(2 * arr[x] + 1, 3 * arr[y] + 1);
+      if (arr[i] == 2 * arr[x] + 1) x++;
+      if (arr[i] == 3 * arr[y] + 1) y++;
+    }
+
+    return arr[n];
+  
 }

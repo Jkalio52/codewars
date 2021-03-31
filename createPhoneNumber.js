242075d3d0083d0c39fe6242a087615aa4ac69e7
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-03-30 19:44:14 -0500

    Create createPhoneNumber.js
    Create Phone Number!!
    
    Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
    
    Example:
    createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
    The returned format must be correct in order to complete this challenge.
    Don't forget the space after the closing parentheses!
    
    Tags: 
    ALGORITHMS, ARRAYS, STRINGS, LOOPS, CONTROL-FLOW, BASIC LANGUAGE FEATURES, FUNDAMENTALS, FORMATTIN, GREGULAR EXPRESSIONS, DECLARATIVE PROGRAMMING, ADVANCED LANGUAGE FEATURES

---
+++ createPhoneNumber.js
@@ -0,0 +1,15 @@
+function createPhoneNumber(numbers){
+  // Solution with .split() and .join() methods
+  //return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`
+  
+  
+  
+  // Solution with .replace() method
+  let tel = '(xxx) xxx-xxxx';
+  
+  for (let i = 0; i < numbers.length; i++) {
+    tel = tel.replace('x', numbers[i]);
+  }
+  
+  return tel;
+}

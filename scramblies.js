a064146ad13d77b78e7435f8b2bae0dcaeae55b4
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-04-01 17:08:07 -0500

    Create scramblies.js
    Scramblies!
    
    Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
    
    Notes:
    
    Only lower case letters will be used (a-z). No punctuation or digits will be included.
    Performance needs to be considered
    Input strings s1 and s2 are null terminated.
    Examples
    scramble('rkqodlw', 'world') ==> True
    scramble('cedewaraaossoqqyt', 'codewars') ==> True
    scramble('katas', 'steak') ==> False
    
    Tags:
    ALGORITHMS, STRINGS, PERFORMANCE OPTIMIZATION

---
+++ scramblies.js
@@ -0,0 +1,12 @@
+function scramble(str1, str2) {
+ // code me
+  if (str1.length < str2.length) return false;
+  
+  const word2 = str2.split("");
+  
+  str1.split("").forEach(val => {
+    if (word2.includes(val)) word2.splice(word2.indexOf(val), 1);
+  });
+
+    return word2.length == 0;
+}

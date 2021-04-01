948459fc14e2d608d2df200daaecd8b274002838
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-04-01 17:58:59 -0500

    Create chain_adding_function.js
    A Chain Adding Function!!
    
    We want to create a function that will add numbers together when called in succession.
    
    add(1)(2);
    // returns 3
    We also want to be able to continue to add numbers to our chain.
    
    add(1)(2)(3); // 6
    add(1)(2)(3)(4); // 10
    add(1)(2)(3)(4)(5); // 15
    and so on.
    
    A single call should return the number passed in.
    
    add(1); // 1
    We should be able to store the returned values and reuse them.
    
    var addTwo = add(2);
    addTwo; // 2
    addTwo + 5; // 7
    addTwo(3); // 5
    addTwo(3)(5); // 10
    We can assume any number being passed in will be valid whole number.
    
    Tags:
    PUZZLES, ARITHMETIC, MATHEMATICS, ALGORITHMS, NUMBERS, FUNCTIONS, CONTROL FLOW, BASIC LANGUAGE FEATURES, FUNDAMENTALS, ADVANCED LANGUAGE FEATURES, FUNCTIONAL PROGRAMMING, DECLARATIVE PROGRAMMING

---
+++ chain_adding_function.js
@@ -0,0 +1,14 @@
+function add(n){
+  // Let the currying begin!
+  const self = function(m) {
+	  return add(n + m);
+	};
+	
+	self.valueOf = function() {
+	  return n;
+	};
+	
+	return self;
+}
+
+add(1);

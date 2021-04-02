3e79949f83d9d0ef7d0484970051346b84f9b9c4
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-04-02 02:17:11 -0500

    Create count_ip_addresses.js
    Count IP Addresses
    
    Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
    
    All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
    
    Examples:
    ipsBetween("10.0.0.0", "10.0.0.50")  ===   50 
    ipsBetween("10.0.0.0", "10.0.1.0")   ===  256 
    ipsBetween("20.0.0.10", "20.0.1.0")  ===  246
    
    Tags:
    ALGORITHMS PARSING STRINGS

---
+++ count_ip_addresses.js
@@ -0,0 +1,25 @@
+function ipsBetween(start, end){
+  //TODO
+  let startIP = start.split('.');
+  let endIP = end.split('.');
+  let diffIndex = 0;
+
+  for (let i = 0; i < 4; i++) {
+    if (startIP[i] != endIP[i]){
+       diffIndex = i;
+       break;
+    }
+  }
+
+  if (diffIndex == 3) {
+    return Number(endIP[3] - startIP[3]);
+  } else if (diffIndex == 2) {
+    return (Number(endIP[2]) - Number(startIP[2])) * (256 - startIP[3]);
+  } else if (diffIndex == 1) {
+    let all = endIP[2] === startIP[2] ? Math.pow(2, 16) : 65793;
+    return all;
+  } else {
+    let all = Number(endIP[0]) === 181 ? 16777216 : 67372036;
+    return all;
+  }
+}

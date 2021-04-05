83a2bab2d496534d1b281cb5b3cd3d487c263bde
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-04-05 18:43:13 -0500

    Create merged_string_checker.js
    Merged String Checker!!!
    
    At a job interview, you are challenged to write an algorithm to check if a given string, s, can be formed from two other strings, part1 and part2.
    
    The restriction is that the characters in part1 and part2 should be in the same order as in s.
    
    The interviewer gives you the following example and tells you to figure out the rest from the given test cases.
    
    For example:
    
    'codewars' is a merge from 'cdw' and 'oears':
    
        s:  c o d e w a r s   = codewars
    part1:  c   d   w         = cdw
    part2:    o   e   a r s   = oears
    
    Tags:
    ALGORITHMS, STRINGS

---
+++ merged_string_checker.js
@@ -0,0 +1,17 @@
+function isMerge(s, part1, part2) {
+  // Divide the strings into ordered list of substrings
+  let input = s.split('');
+  let firstWrd = part1.split('');
+  let secondWrd = part2.split('');
+  
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === firstWrd[0]) {
+      firstWrd.splice(0, 1);
+    } else if (input[i] === secondWrd[0]) {
+      secondWrd.splice(0, 1);
+    } else {
+      return false;
+    }
+  }
+  return true;
+}

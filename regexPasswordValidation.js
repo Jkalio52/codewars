fe8416ede53b4d7f16c28ce7e5c2189dd0de0bef
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-04-07 13:21:06 -0500

    Update regexPasswordValidation.js

--- regexPasswordValidation.js
+++ regexPasswordValidation.js
@@ -3,3 +3,26 @@ function validate(password) {
   
   //return /(put answer here)/.test(password);
 }
+
+
+// Another solution
+function validate(password) {
+  let minMaxLength = /^[\s\S]{8,32}$/,
+        upper = /[A-Z]/,
+        lower = /[a-z]/,
+        number = /[0-9]/,
+        special = /[^A-Za-z0-9]/,
+        count = 0;
+
+    if (minMaxLength.test(password)) {
+        // Only need 3 out of 4 of these to match
+        if (upper.test(password)) count++;
+        if (lower.test(password)) count++;
+        if (number.test(password)) count++;
+        if (special.test(password)) count++;
+    }
+
+    return count >= 3;
+  
+  //return /(put answer here)/.test(password);
+}

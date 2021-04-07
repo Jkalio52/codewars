f17508b859df9f1b69926682aa24b45f2e232a4e
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-04-06 22:58:59 -0500

    Create extract_domain_names_from_url.js
    Extract Domain Names from URLs!
    
    Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
    
    domainName("http://github.com/carbonfive/raygun") == "github" 
    domainName("http://www.zombie-bites.com") == "zombie-bites"
    domainName("https://www.cnet.com") == "cnet"
    
    Tags:
    FUNDAMENTALS, PARSING, ALGORITHMS, STRINGS, REGULAR EXPRESSIONS, DECLARATIVE PROGRAMMING, ADVANCED LANGUAGE FEATURES

---
+++ extract_domain_names_from_url.js
@@ -0,0 +1,21 @@
+function domainName(url) {
+  //your code here
+  let x = /(\/{2}|\.)/g;
+  let domain = url.match(x);
+  let str = '';
+  
+  if (domain[0] == '//') {
+    if (url.charAt(url.indexOf(domain[0]) + 2) == "w") {
+      str = url.slice(url.indexOf(domain[1]) + 1, url.indexOf(domain[1], url.indexOf(domain[1])+1));
+    }
+    else {
+      str = url.slice(url.indexOf(domain[0]) + 2, url.indexOf(domain[1]));
+    }
+    
+  }
+    else {
+      str = url.slice(url.indexOf(domain[0]) + 1, url.lastIndexOf(domain[1]));
+    }
+  
+    return str;
+}

9e43eb668d3fe3f85a4c3c78a4e8cd4ca8b44d10
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-04-07 15:05:05 -0500

    Create nextBiggerNumber.js
    Next Bigger Number with the same Digits!!
    
    Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
    
    12 ==> 21
    513 ==> 531
    2017 ==> 2071
    nextBigger(num: 12)   // returns 21
    nextBigger(num: 513)  // returns 531
    nextBigger(num: 2017) // returns 2071
    If the digits can't be rearranged to form a bigger number, return -1 (or nil in Swift):
    
    9 ==> -1
    111 ==> -1
    531 ==> -1
    nextBigger(num: 9)   // returns nil
    nextBigger(num: 111) // returns nil
    nextBigger(num: 531) // returns nil
    
    Tags:
    REFACTORING NUMBERS STRINGS INTEGERS

---
+++ nextBiggerNumber.js
@@ -0,0 +1,10 @@
+function nextBigger(n) {
+  //your code here
+  const posInteger = n => ('' + n).split('').sort((a, b) => b - a)
+  const biggestNum = +posInteger(n).join('')
+  for (let i = n + 1; i <= biggestNum; i++) {
+    if (biggestNum === +posInteger(i).join('')) return i
+  }
+  
+  return -1
+}

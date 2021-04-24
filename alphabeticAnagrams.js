884f2645ed9aa37f9900f7b4a855f22c49f58684
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-04-24 02:22:30 -0500

    Update alphabeticAnagrams.js

--- alphabeticAnagrams.js
+++ alphabeticAnagrams.js
@@ -1,1 +1,29 @@
+function listPosition(word) {
+  let obj = {};
+  let counter = [];
+  let counts = 0;
+
+  word.split("").sort().forEach(function(x) {
+      if ( obj[x] == undefined ) {
+          obj[x] = counts;
+          counter[counts] = 0;
+          counts ++;
+      }
+  });
+
+  let min = 1;
+  let sum = min;
+
+  word.split("").reverse().forEach(function(x, i) {
+      let step = i + 1, idx = obj[x];
+      counter[idx] ++;
+      min /= counter[idx];
+      for (let j = 0; j < idx; ++j) 
+          if (counter[j] != 0) 
+              sum += min * counter[j];
+      min *= step;
+  });
+
+  return sum;
+}
 

3f35dbaba5a3dc8f3beb8073667a0b29637232f6
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-05-06 20:32:05 -0500

    Update strip-comments.js

--- strip-comments.js
+++ strip-comments.js
@@ -12,3 +12,9 @@ function solution(input, markers) {
   }
   return comments.join('\n');
 };
+
+
+
+// Best Practice by 0lexa
+const solution = (input, markers) =>
+  input.replace(new RegExp(`\\s*[${markers.join(`|`)}].*`, `g`), ``);

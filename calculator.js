27652563dd7dc45d713826513cb416bf77e6de54
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-05-18 22:11:45 -0500

    Create calculator.js
    Calculator!!
    
    Create a simple calculator that given a string of operators (), +, -, *, / and numbers separated by spaces returns the value of that expression
    
    Example:
    
    Calculator().evaluate("2 / 2 + 3 * 4 - 6") # => 7
    Remember about the order of operations! Multiplications and divisions have a higher priority and should be performed left-to-right. Additions and subtractions have a lower priority and should also be performed left-to-right.
    
    Tags:
    ALGORITHMS PARSING STRINGS EXPRESSIONS BASIC LANGUAGE FEATURES FUNDAMENTALS

---
+++ calculator.js
@@ -0,0 +1,28 @@
+const Calculator = function() {
+  this.evaluate = string => {
+    // do code here    
+    //let numbers = string.split(/,|\n/).map(Number)
+  //return numbers.reduce((a, b) => a + b)
+    
+    let arr = string.split(' ');
+    while (arr.length > 1) {
+      // multiplication and division have priority
+      // create a new array that will only have numbers and + or -
+      const ops = arr.findIndex(a => a === "*" || a === "/");
+      const index =
+        ops === -1 ? arr.findIndex(b => b === "+" || b === "-") : ops;
+      const a = Number(arr[index - 1]);
+      const b = Number(arr[index + 1]);
+      const calc =
+        arr[index] === "/"
+          ? a / b
+          : arr[index] === "*"
+          ? a * b
+          : arr[index] === "-"
+          ? a - b
+          : a + b;
+      arr.splice(index - 1, 3, calc);
+    }
+    return Number(arr[0])
+  };
+};

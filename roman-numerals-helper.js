f23851044652895342d5d6ba10d91480d47a59bc
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-05-25 22:51:14 -0500

    Create roman-numerals-helper.js
    Roman Numerals Helper!!!
    
    Create a RomanNumerals class that can convert a roman numeral to and from an integer value. It should follow the API demonstrated in the examples below. Multiple roman numeral values will be tested for each helper method.
    
    Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
    
    Examples
    RomanNumerals.toRoman(1000); // should return 'M'
    RomanNumerals.fromRoman('M'); // should return 1000
    
    Help
    Symbol	Value
    I	        1
    V	        5
    X	        10
    L	        50
    C	        100
    D	        500
    M	        1000
    
    Tags:
    ALGORITHMS - PARSING - STRINGS - TYPE CONVERSION

---
+++ roman-numerals-helper.js
@@ -0,0 +1,63 @@
+// TODO: create a RomanNumerals helper object
+const RomanNumerals = {
+  toRoman: function(value) {
+    let map = { 
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9, 
+      V: 5,
+      IV: 4,
+      I: 1 
+    };
+    
+    let roman = '';
+    while (value > 0) {
+      for (var r in map) {
+        if (map[r] <= value) {
+          roman += r;
+          value -= map[r];
+          break;
+        }
+      }
+    }
+    
+    return roman;
+  },
+  
+  fromRoman: function(roman) {
+    let map = { 
+      IV: 4,
+      IX: 9,
+      XL: 40,
+      XC: 90,
+      CD: 400,
+      CM: 900,
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000 
+    };
+    
+    let value = 0;
+    for (var i = 0; i < roman.length; i++) {
+      let two = map[roman[i] + roman[i + 1]], one = map[roman[i]];
+      if (two != null) { 
+        value += two; 
+        i++; 
+      } else if (one != null) value += one;
+    }
+    
+    return value;
+  }
+};

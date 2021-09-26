695b67b5b6cf1b55639eb08838de79b68b60d9c6
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2021-09-26 17:54:15 -0500

    Update stringsMix.js

--- stringsMix.js
+++ stringsMix.js
@@ -35,7 +35,6 @@ function mix(s1, s2) {
 
 
 
-
 // Solution by 0lexa
 const mix = (s1, s2) =>
   [...`abcdefghijklmnopqrstuvwxyz`].map(val => [val, s1.split(val).length - 1, s2.split(val).length - 1])

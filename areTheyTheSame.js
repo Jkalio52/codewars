ae0db007e8090b57b89ea5e9e8fdc5088acd7736
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2024-03-15 11:44:00 -0500

    Update areTheyTheSame.js

--- areTheyTheSame.js
+++ areTheyTheSame.js
@@ -1,5 +1,5 @@
 function comp(array1, array2){
-  //your code here
+  // your code here
   if (!array1 || !array2 || array1.length !== array2.length) return false;
   
   return array1.map(x => x * x).sort().toString() === array2.sort().toString();

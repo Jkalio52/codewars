3d3d1f43387764cad4b6b22f0034af20b98c7149
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2024-08-03 13:57:29 -0500

    Update remove_zeros.js

--- remove_zeros.js
+++ remove_zeros.js
@@ -1,6 +1,6 @@
 function removeZeros(array) {
   // Sort "array" so that all elements with the value of zero are moved to the 
-  // end of the array, while the other elements maintain order.
+  // end of the array, while the other elements maintain order. 
   // [0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0]
   // Zero elements also maintain order in which they occurred.
   // [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"]

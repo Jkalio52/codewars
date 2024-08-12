3ec48595de72a72a205ec866d2270930240c2cd5
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2024-08-12 07:56:16 -0500

    Update helpTheGeneralDecodeSecretEnemyMessages.js

--- helpTheGeneralDecodeSecretEnemyMessages.js
+++ helpTheGeneralDecodeSecretEnemyMessages.js
@@ -43,7 +43,7 @@ device.decode = function (w) {
 
 
 
-// Clean and easy to read solution by 0lexa
+// Clean and easy to read solution by 0lexa 
 device.decode = w =>
   [...Array(65)].reduce(pre => device.encode(pre), w);
 

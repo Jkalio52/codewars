1d829de34a147955c4075bc54e60cef63902204e
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2024-08-14 09:31:34 -0500

    Update findTheNextPerfectSquare.js

--- findTheNextPerfectSquare.js
+++ findTheNextPerfectSquare.js
@@ -28,7 +28,7 @@ function findNextSquare(sq) {
 
 
 
-// Other solution with ternary operator
+// Other solution with ternary operator 
 function findNextSquare(sq) {
   var root = Math.sqrt(sq);
   return root % 1 === 0 ? Math.pow(root + 1, 2) : -1;

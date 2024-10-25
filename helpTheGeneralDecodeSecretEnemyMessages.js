e8ab124bbad72e7f451db910d51b242f40e9ec2f
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2024-10-25 09:00:25 -0500

    Update helpTheGeneralDecodeSecretEnemyMessages.js

--- helpTheGeneralDecodeSecretEnemyMessages.js
+++ helpTheGeneralDecodeSecretEnemyMessages.js
@@ -32,7 +32,7 @@ device.decode = function (w) {
 
 
 
-// Other solution by firstonline, MaxiimRandob, d@rude1337 -- Very easy to read :) 
+// Other solution by firstonline, MaxiimRandob, d@rude1337 -- Very easy to read :)
 device.decode = function (w) {
   for (j = 0; j < 65; j++){
     w = device.encode (w)

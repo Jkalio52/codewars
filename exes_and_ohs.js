29f0a6ce7c99a5be1ccd8854ef81b5e20a3d857b
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2025-03-26 10:49:35 -0500

    Update exes_and_ohs.js

--- exes_and_ohs.js
+++ exes_and_ohs.js
@@ -32,7 +32,7 @@ function XO(str) {
 
 
 
-// Other solution I found, looks a lot simpler...
+// I found another solution, and it looks a lot simpler.
 function XO(str) {
     return str.toLowerCase().split('x').length === str.toLowerCase().split('o').length;
 }

2c85837812306f81ed17ec3f07060b90ffc34a8c
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2025-05-22 10:32:02 -0500

    Update remove_zeros.js

--- remove_zeros.js
+++ remove_zeros.js
@@ -3,7 +3,7 @@ function removeZeros(array) {
   // end of the array, while the other elements maintain order. 
   // [0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0] 
   // Zero elements also maintain order in which they occurred. 
-  // [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"]
+  // [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"] 
   
   // Do not use any temporary arrays or objects. Additionally, you're not able 
   // to use any Array or Object prototype methods such as .shift(), .push(), etc 

95dc43a3df9e5493572d9ac6ce0df5544ebe089c
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2025-12-14 10:44:15 -0600

    Add variable in bitCounting.js

--- bitCounting.js
+++ bitCounting.js
@@ -1,6 +1,6 @@
 var countBits = function(n) {
   // Program Me 
-  // Create an array with bit output
+  // Create an array with bit output 
    let binary = (n).toString(2).split('');
    
    // Then create a sum with the array and make the index a Number 

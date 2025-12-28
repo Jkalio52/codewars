88d683a50dc718a6dfcb00e5c7c828b7d45faae9
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2025-12-28 11:46:40 -0600

    Fix comment wording in clockHands.js
    Corrected wording in the comment for clarity.

--- clockHands.js
+++ clockHands.js
@@ -1,5 +1,5 @@
 /**
-In some parts of the world, you might still find round analog clocks with hour, minute, and second hands. Such clocks are divided up into 12 hours, with a 12 at the top and a 6 at the bottom.
+In some parts of the world, you might still find round analog clocks with hour, minute, and second hands. Such clocks are divided into 12 hours, with a 12 at the top and a 6 at the bottom.
 
 Write a function that produces a sequence of successive times in which the hour and minute hand make a given angle, which will be given in degrees, measured clockwise from the hour hand to the minute hand. 
 The times must be formatted as HH:MM:SS, zero-padding all time components. Use 12 (not 00) for the hour component for midnight. If the actual time is in between two seconds, choose the smaller time value.

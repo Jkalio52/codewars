d8ad25bf7365b7dfafe0e80dbf606fe83a5de96c
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-01-02 10:18:37 -0600

    Add comment grammar in removeZeros function
    Corrected the wording for clarity in comments.

--- remove_zeros.js
+++ remove_zeros.js
@@ -2,7 +2,7 @@ function removeZeros(array) {
   // Sort "array" so that all elements with the value of zero are moved to the 
   // end of the array, while the other elements maintain order. 
   // [0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0] 
-  // Zero elements also maintain order in which they occurred. 
+  // Zero elements also maintain the order in which they occurred. 
   // [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"] 
   
   // Do not use any temporary arrays or objects. Additionally, you're not able 

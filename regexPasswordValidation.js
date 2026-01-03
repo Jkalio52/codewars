7b9953fb8f5e0a0f2c0c1a9963130054e265225a
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-01-03 09:43:56 -0600

    Update regexPasswordValidation.js
    Replaced the return statement inside the validate function.

--- regexPasswordValidation.js
+++ regexPasswordValidation.js
@@ -1,7 +1,7 @@
 function validate(password) {
   return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/.test(password);
   
-  //return /(put answer here)/.test(password); 
+  // return /(put answer here)/.test(password); 
 }
 
 

639854490c799d849b4b4694082c9dd086c15af7
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-02-17 09:09:57 -0600

    Cleanup: Remove commented-out code in nestingStructureComparison.js
    Removed commented-out alternative solution for structure comparison.

--- nestingStructureComparison.js
+++ nestingStructureComparison.js
@@ -20,17 +20,3 @@ Array.prototype.sameStructureAs = function (other) {
   }
   return true;
 };
-
-
-
-
-
-
-
-// Other solution...
-
-Array.prototype.sameStructureAs = function (other) {
-  if (!isArray(other)) return false;
-
-  return this.join("").replace(/[^.,]/g, 1) === other.join("").replace(/[^.,]/g, 1);
-};

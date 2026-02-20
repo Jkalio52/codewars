2c485e3acc5a649df4f9997372130e1006a68172
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-02-20 07:13:58 -0600

    Enhance instructions with valid input examples
    Added examples of valid IPv4 inputs to instructions.

--- ip-validation.js
+++ ip-validation.js
@@ -1,6 +1,7 @@
 /**
 Instructions: 
-Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
+Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. 
+IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
 
 Valid input examples:
 Examples of valid inputs:

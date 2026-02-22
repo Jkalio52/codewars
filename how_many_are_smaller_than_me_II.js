d4b4ebcc6d73f489fe7ec21c290ae65597ce305c
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-02-22 12:03:11 -0600

    Enhance comments in smaller function
    Add comment to explain the use of .slice in smaller function

--- how_many_are_smaller_than_me_II.js
+++ how_many_are_smaller_than_me_II.js
@@ -1,7 +1,7 @@
 function smaller(arr) {
   // .map iterates through each element 'x' at index 'y'
   return arr.map((x, y) => {
-    // 
+    // .slice(y) creates a new sub-array from the current position to the end
     return arr.slice(y).filter(num => num < x).length;
   });
 }

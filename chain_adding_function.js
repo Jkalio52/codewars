98b29227a5d88449a11327ebb059241f3293e958
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-03-08 10:37:04 -0500

    Implement currying in the add function
    Refactor the add function to support currying and closure.

--- chain_adding_function.js
+++ chain_adding_function.js
@@ -12,3 +12,22 @@ function add(n) {
 }
 
 add(1);
+
+
+//* Updated */
+function add(n) {
+  // Let the currying begin!
+  // This inner function captures 'n' in its closure
+  const self = function(m) {
+    // When called, it returns a NEW instance of add with the updated sum
+    return add(n + m);
+  };
+  
+  // This overrides the default object-to-primitive conversion
+  // It allows 'add(1)(2)' to be treated as a number in math operations
+  self.valueOf = function() {
+    return n;
+  };
+  
+  return self;
+}

c85ff29159a21bc8adc0c2bb67df0ffa4cffee50
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-03-19 08:25:22 -0500

    Fix formatting issues in CuckooClock.js comments

--- CuckooClock.js
+++ CuckooClock.js
@@ -13,7 +13,7 @@ If the cuckoo bird chimes at initial_time, include those chimes in the count. If
 Example: "03:38", 19 should return "06:00".
 Explanation: It chimes once at "03:45",4 times at "04:00", once each at "04:15", "04:30", "04:45", 5 times at "05:00", and once each at "05:15", "05:30", "05:45". At this point it has chimed 16 times, so the 19th chime occurs when it chimes 6 times at "06:00".
 
-Source: International Collegiate Programming Contest, North Central North American Regional, 2023.
+Source: International Collegiate Programming Contest, North Central North American Regional, 2023. 
 */
 
 function cuckooClock(inputTime, chimes) {

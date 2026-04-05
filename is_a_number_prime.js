49a9a74dc76ac0d9663938d16009e8f98f3d3f90
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-04-05 10:03:46 -0500

    Clarify loop purpose in isPrime function
    Added a comment to clarify the loop's purpose.

--- is_a_number_prime.js
+++ is_a_number_prime.js
@@ -1,5 +1,6 @@
 function isPrime(num) {
   // TODO 
+  // Loop starts at 2 and checks every single integer up to 'num'
   for (let i = 2; i < num; i++)
     if (num % i === 0) return false;
   return num > 1;

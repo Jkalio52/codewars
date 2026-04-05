66ac9a56a13ec00923b28267febdf95007148179
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-04-05 10:04:38 -0500

    Add prime number checking logic
    Implement prime number checking logic in isPrime function.

--- is_a_number_prime.js
+++ is_a_number_prime.js
@@ -2,6 +2,7 @@ function isPrime(num) {
   // TODO 
   // Loop starts at 2 and checks every single integer up to 'num'
   for (let i = 2; i < num; i++)
+    // If 'num' is divisible by 'i' with no remainder, it's not prime
     if (num % i === 0) return false;
   return num > 1;
 }

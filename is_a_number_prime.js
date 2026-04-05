7172d696f1806cffb45ab80d898261e550f09c6d
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-04-05 10:05:16 -0500

    Clarify prime number condition in is_a_number_prime.js
    Added a comment to clarify that primes must be greater than 1.

--- is_a_number_prime.js
+++ is_a_number_prime.js
@@ -4,5 +4,7 @@ function isPrime(num) {
   for (let i = 2; i < num; i++)
     // If 'num' is divisible by 'i' with no remainder, it's not prime
     if (num % i === 0) return false;
+
+  // Primes must be greater than 1
   return num > 1;
 }

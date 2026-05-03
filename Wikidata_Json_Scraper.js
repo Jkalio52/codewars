82aa100e5e8bf0194e656216926ee7b3532ef12a
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-05-03 11:33:54 -0500

    Add documentation for Wikidata JSON scraper
    Added background and instructions for scraping Wikidata JSON data.

--- Wikidata_Json_Scraper.js
+++ Wikidata_Json_Scraper.js
@@ -1,1 +1,39 @@
+/**
+Background:
+Wikidata is a public database with over a hundred million entries in it. You can find almost anything documented, from scientific articles to new species scientists have found.
 
+To help developers use the site better, the website provides accessible JSON data for every submitted item. Today, you are going to program something that can read that data.
+
+The link below leads to the page explaining how to access the corresponding JSON address of a specific Wikidata item. Although the direct URL to the data will be provided (example), you might find it helpful to see the stuff your collecting from.
+
+https://www.wikidata.org/wiki/Wikidata:Data_access
+
+Now let's move on to the actual scraping.
+
+What to do:
+Take a look at this labeled section of a Wikidata page:
+
+alt caption: Identifier = Q42, Label = Douglas Adams, Description = English Writer and Humorist
+
+All of this information can be found on the given JSON pages (link in the previous section). Your function should return a dictionary with the English (en) values of the Identifier, Label, and Description. The dictionary should have the keys "ID", "LABEL", and "DESCRIPTION", respectively.
+
+Given the URL to our example, you should return:
+
+{
+  'ID': 'Q42',
+  'LABEL': 'Douglas Adams',
+  'DESCRIPTION': 'British science fiction writer and humorist (1952–2001)'
+}
+Be careful. Some information won't always be available in English, and some might not have a value associated with any language. That's because Wikidata only requires you to give one label or description. If the en label or description aren't included, put "No Label" or "No Description". Please be careful you are getting values from the "en" value, and not localized english versions such as "en-uk" or en-ca.
+
+If you were given the URL to this unhelpful entry, you would return the following:
+
+{
+  "ID": "Q97226458",
+  "LABEL": "No Label",
+  "DESCRIPTION": "No Description"
+}
+That should be it for instructions. If you have any more questions, check out the section below, or ask them in the Discourse (after you read the section below please.)
+
+Good Luck!
+*/

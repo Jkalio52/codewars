e0b868c80479983eeacca66684cc2f45719a76e6
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-05-13 07:04:26 -0500

    Update comment in isPrime function

--- is_a_number_prime.js
+++ is_a_number_prime.js
@@ -1,6 +1,6 @@
 function isPrime(num) {
   // TODO 
-  // Loop starts at 2 and checks every single integer up to 'num'
+  // Loop starts at 2 and checks every single integer up to 'num' 
   for (let i = 2; i < num; i++)
     // If 'num' is divisible by 'i' with no remainder, it's not prime
     if (num % i === 0) return false;

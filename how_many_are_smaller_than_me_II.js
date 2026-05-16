dd41beebd06ed6dcccc23078f50a922fd4ee604c
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-05-16 09:54:38 -0500

    Update smaller function

--- how_many_are_smaller_than_me_II.js
+++ how_many_are_smaller_than_me_II.js
@@ -1,5 +1,5 @@
 function smaller(arr) {
-  // .map iterates through each element 'x' at index 'y'
+  // .map iterates through each element 'x' at index 'y' 
   return arr.map((x, y) => {
     // .slice(y) creates a new sub-array from the current position to the end
     // .filter(num => num < x) creates another array of only smaller numbers

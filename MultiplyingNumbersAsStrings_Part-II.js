db72087c7e6f556d5aa7bc825baef0155806e615
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-05-24 11:44:33 -0500

    Add documentation for multiplying numbers as strings
    Added a comment block outlining the purpose and rules for multiplying numbers as strings.

--- MultiplyingNumbersAsStrings_Part-II.js
+++ MultiplyingNumbersAsStrings_Part-II.js
@@ -1,1 +1,16 @@
+/**
+Multiplying numbers as strings: Part II
 
+This is the Part II of Multiplying numbers as strings.
+
+TODO
+Multiply two numbers! Simple!
+
+Rules
+1. The arguments are passed as strings.
+2. The numbers will be very large
+3. The arguments can be negative, in decimals, and might have leading and trailing zeros. e.g. "-01.300"
+4. Answer should be returned as string
+5. The returned answer should not have leading or trailing zeroes (when applicable) e.g. "0123" and "1.100" are wrong, they should be "123" and "1.1"
+6. Zero should not be signed and "-0.0" should be simply returned as "0".
+*/

e677455fc49ad8ce3d158dfd2bed916a8ba344ac
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-05-31 14:09:22 -0500

    Update instructions and hints in hello-world task

--- multi-line-task_hello-world.js
+++ multi-line-task_hello-world.js
@@ -2,9 +2,9 @@
 Instructions: 
 You need to write a function f that returns the string Hello, world!.
 
-Requirement: Every line must have at most 2 characters, and total number of lines must be less than 40.
+Requirement: Every line must have at most 2 characters, and total number of lines must be less than 40. 
 
-Hint: It's possible to complete this in 28 lines only.
+Hint: It's possible to complete this in 28 lines only. 
 
 
 Solution: 

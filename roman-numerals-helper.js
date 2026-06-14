7ec2cfd162385f8838d30da6c675ff21e0f458d5
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-06-14 13:12:11 -0500

    Refactor Roman numeral helper functions
    Refactor Roman numeral conversion functions to use a single object for mapping values and improve readability.

--- roman-numerals-helper.js
+++ roman-numerals-helper.js
@@ -74,4 +74,34 @@ const RomanNumerals = {
 /**
 Here we leverage Array.entries(), reduce(), and for...of loops to make the code more declarative, readable, and less prone to the "index tracking" bugs
 */
+const RomanNumerals = {
+  // Define a single source of truth for the values
+  map: [
+    ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
+    ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
+    ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]
+  ],
 
+  toRoman(value) {
+    let result = '';
+    for (const [symbol, number] of this.map) {
+      while (value >= number) {
+        result += symbol;
+        value -= number;
+      }
+    }
+    return result;
+  },
+
+  fromRoman(roman) {
+    // Map individual chars to values for the lookahead logic
+    const charMap = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    
+    return [...roman].reduce((acc, char, i, arr) => {
+      const current = charMap[char];
+      const next = charMap[arr[i + 1]];
+      // If the current value is less than the next, subtract it (like IV = 4)
+      return next > current ? acc - current : acc + current;
+    }, 0);
+  }
+};

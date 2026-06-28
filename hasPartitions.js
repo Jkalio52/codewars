7670529b41e6ac82bb4b1d7fe960b2ad9b7bfd78
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-06-28 15:02:07 -0500

    Add instructions for hasPartitions function
    Added instructions and initial comments for hasPartitions function.

--- hasPartitions.js
+++ hasPartitions.js
@@ -1,1 +1,61 @@
+/**
+# Instructions:
 
+Task
+Can you decide whether or not some walls divide a rectangular room into two or more partitions?
+
+
+Input
+width - The width of the room
+
+height - The height of the room
+
+walls - A list of line segments
+
+
+Output
+true if the walls divide the room into two or more partitions, otherwise false.
+
+
+Examples
+oops, please raise an issue about this broken image
+
+oops, please raise an issue about this broken image
+
+oops, please raise an issue about this broken image
+
+Notes:
+line segments are given as pairs of points
+
+Tests:
+50 fixed tests
+100 random tests with up to 8 walls
+100 random with up to 15 walls
+100 randrom with up to 100 walls
+points and walls may exist outside of boundary walls. But any partitions formed outside do NOT count.
+
+the coordinate (width, height) is a corner of the room, and so is (0, 0).
+
+reading the pictures: origin in the top left corner, x axis going to the right, and y axis going down.
+
+tests have up to 100 walls
+
+make sure your arithmetic avoids floating point errors.
+
+there may be duplicate line segments
+
+
+Enjoy!
+
+
+Tags: Geometry
+
+
+
+# Solution (starting code): 
+
+function hasPartitions(width, height, walls) {
+  // TODO: DO IT
+}
+
+*/

1936415318fedb6e88eadade7b95d24be2db33dc
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-07-05 11:42:59 -0500

    Add instructions for circle intersection task
    Added instructions and task description for circle intersection problem.

--- circleIntersection.py
+++ circleIntersection.py
@@ -1,1 +1,22 @@
+#
+# Instructions: 
+
+# Task:
+# Given two congruent circles a and b of radius r, return the area of their intersection rounded down to the nearest integer.
+
+# Code Limit:
+# JavaScript: Less than 94 characters.
+
+# Python: Less than 128 characters.
+
+# Example
+# For c1 = [0, 0], c2 = [7, 0] and r = 5,
+
+# the output should be 14.
+
+# Tags: Puzzles Restricted 
+
+
+# Solution (starting code): 
+# circleIntersection=(a,b,r)=>//coding and coding.. shorter and more shorter..  good luck! ;-) 
 

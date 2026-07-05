2edd4d7a28abe7f48031c33629a3219ba33de8ee
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-07-05 14:40:43 -0500

    Refactor circleIntersection function for clarity

--- circleIntersection.js
+++ circleIntersection.js
@@ -23,4 +23,6 @@ circleIntersection=(a,b,r)=>//coding and coding.. shorter and more shorter..  go
 */
 
 // Strict-mode safe code matching the exact mandated function signature
-circleIntersection=(a,b,r)=>(X=Math.hypot(a[0]-b[0],a[1]-b[1])/2/r)>1?0:r*r*(2*Math.acos(X)-2*X*Math.sqrt(1-X*X))|0
+// circleIntersection=(a,b,r)=>(X=Math.hypot(a[0]-b[0],a[1]-b[1])/2/r)>1?0:r*r*(2*Math.acos(X)-2*X*Math.sqrt(1-X*X))|0
+
+with(Math)circleIntersection=([a,b],[c,d],r)=>(-sin(x=2*acos(hypot(a-c,b-d)/2/r))+x)*r*r|0

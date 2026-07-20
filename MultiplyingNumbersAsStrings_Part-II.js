e1ea9225a30e167aa72c7f01ac3adf0e1a7709ca
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-07-20 10:36:54 -0500

    Fix formatting in comments for clarity

--- MultiplyingNumbersAsStrings_Part-II.js
+++ MultiplyingNumbersAsStrings_Part-II.js
@@ -11,7 +11,7 @@ Rules
 2. The numbers will be very large
 3. The arguments can be negative, in decimals, and might have leading and trailing zeros. e.g. "-01.300"
 4. Answer should be returned as a string
-5. The returned answer should not have leading or trailing zeroes (when applicable) e.g. "0123" and "1.100" are wrong, they should be "123" and "1.1"
+5. The returned answer should not have leading or trailing zeroes (when applicable) e.g. "0123" and "1.100" are wrong; they should be "123" and "1.1"
 6. Zero should not be signed and "-0.0" should be simply returned as "0".
 
 Tags: Strings BigIntegers Fundamentals

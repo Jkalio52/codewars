4edc1fbd48e1cfa38623ab5922629ffbc857276d
Jay Kalio <30821820+Jkalio52@users.noreply.github.com>
2026-07-30 08:02:39 -0500

    Update sameStructureAs function

--- nestingStructureComparison.js
+++ nestingStructureComparison.js
@@ -3,7 +3,7 @@ Array.prototype.sameStructureAs = function (other) {
     // Return 'true' if and only if 'other' has the same 
     // nesting structure as 'this'. 
 
-    // Note: You are given a function isArray(o) that returns
+    // Note: You are given a function isArray(o) that returns 
     // whether its argument is an array.
   if (this.length !== other.length) {
       return false;
